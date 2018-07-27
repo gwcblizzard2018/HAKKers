c0c348ae14e22717b0ccab5e8d06568067e2a208
Mimi Chen <mimichen226@gmail.com>
2018-07-27 12:49:29 -0700

    WOOHOOO

--- glowstuff/test.js
+++ glowstuff/test.js
@@ -8,25 +8,24 @@ document.addEventListener('DOMContentLoaded', function() {
 
 var first_click = true;
 var stuff;
-function glow() {
+function glow(myid) {
     if (first_click) {
 
         var i = 0;
-        var act = document.getElementById('myGlower').getElementsByClassName('carousel-item active');
-        console.log(act);
-        var glow = document.getElementById('myGlower');
-        stuff = setInterval(function(){
-            if(i == 0) {
+        //var act = document.getElementById('myGlower').getElementsByClassName('carousel-item active');
+        //console.log(act);
+        var glow = document.getElementById(myid);
+
+
                 glow.setAttribute('class','active');
-                i++;}
-        },10);
+
         first_click = false;
     } else {
 
-      var glow = document.getElementById('myGlower');
+      var glow = document.getElementById(myid);
       glow.removeAttribute('class');
       console.log(glow);
-      clearInterval(stuff);
+      //clearInterval(stuff);
       first_click = true;
 
       return;
