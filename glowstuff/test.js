5ac9983df81f178a9929f761d7d38cdcf49f5110
Mimi Chen <mimichen226@gmail.com>
2018-07-27 10:06:57 -0700

    Glow up

--- glowstuff/test.js
+++ glowstuff/test.js
@@ -7,12 +7,13 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 var first_click = true;
+var stuff;
 function glow() {
     if (first_click) {
-      console.log("Glowing");
+
         var i = 0;
         var glow = document.getElementById('myGlower');
-        setInterval(function(){
+        stuff = setInterval(function(){
             if(i == 0) {
                 glow.setAttribute('class','active');
                 i++;
@@ -23,22 +24,11 @@ function glow() {
         },500);
         first_click = false;
     } else {
-      console.log("Not glowing")
-//      var i = 0;
+
       var glow = document.getElementById('myGlower');
       glow.removeAttribute('class');
-      /*
-      setInterval(function(){
-          if(i !== 0) {
-              glow.setAttribute('class','active');
-              i++;
-          } else {
-              glow.removeAttribute('class');
-              i = 0;
-          }
-
-
-      },0);*/
+      console.log(glow);
+      clearInterval(stuff);
       first_click = true;
 
       return;
