6f821e96bb066a30ba8f9f98a2c7313409245820
khatuu <41082274+khatuu@users.noreply.github.com>
2018-07-27 11:11:04 -0700

    upup glow

--- glowstuff/test.js
+++ glowstuff/test.js
@@ -12,16 +12,14 @@ function glow() {
     if (first_click) {
 
         var i = 0;
+        var act = document.getElementById('myGlower').getElementsByClassName('carousel-item active');
+        console.log(act);
         var glow = document.getElementById('myGlower');
         stuff = setInterval(function(){
             if(i == 0) {
                 glow.setAttribute('class','active');
-                i++;
-            } else {
-                glow.removeAttribute('class');
-                i = 0;
-            }
-        },500);
+                i++;}
+        },10);
         first_click = false;
     } else {
 
